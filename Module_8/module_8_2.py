eeebca37d18f1c686ced36be635ff38c98e4caea
Aleksei <krasnobaev07@inbox.ru>
2024-10-23 16:56:45 +0500

    Second Commit

--- Module_8/module_8_2.py
+++ Module_8/module_8_2.py
@@ -1,7 +1,6 @@
 def personal_sum(numbers):
     result_ = 0
     incorrect_data = 0
-
     for i  in numbers:
         try:
             result_ += i
@@ -12,7 +11,10 @@ def personal_sum(numbers):
 def calculate_average(numbers : list):
     result_ = 0
     try:
-        result_ = personal_sum(numbers)[0]/len(numbers)
+        sum_num = personal_sum(numbers)[0]
+        length_num = len(numbers) - personal_sum(numbers)[1]
+
+        result_ = sum_num/length_num
     except TypeError as exc:
         print(f'В numbers записан некорректный тип данных')
         return None
@@ -24,4 +26,7 @@ print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка п
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
+# print(personal_sum([1, "Строка", 3, "Ещё Строка"]))
+# print(type(personal_sum([1, "Строка", 3, "Ещё Строка"])))
 
