9ebe14478971e58c463ae8617fa2ca8f84efc484
Aleksei <krasnobaev07@inbox.ru>
2024-12-07 12:32:37 +0500

    First Commit

---
+++ Module_12/module_12_2.py
@@ -0,0 +1,49 @@
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+# run_1 = Runner("Усейн", 10)
+# run_2 = Runner("Андрей", 9)
+# run_3 = Runner("Ник", 3)
+#
+# tour_1 = Tournament(90,(run_1, run_2))
+# print(run_1)
+# print(tour_1.participants)
+# print(tour_1.start())
