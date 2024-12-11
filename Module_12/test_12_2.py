0494d8d25b4cf713e89aeee1665b001c146d32c4
Aleksei <krasnobaev07@inbox.ru>
2024-12-11 16:40:24 +0500

    First Commit

--- Module_12/test_12_2.py
+++ Module_12/test_12_2.py
@@ -1,4 +1,4 @@
-import module_12
+
 import unittest
 
 from Module_12.module_12_2 import Runner, Tournament
@@ -16,26 +16,26 @@ class TournamentTest(unittest.TestCase):
         self.run_3 = Runner("Ник", 3)
 
     @classmethod
-    def tearDownClass(cls):
-        print(cls.all_results)
-
-
-
+    def tearDown(cls):
+        dr = {}
+        for i in cls.all_results:
+            dr[i] = cls.all_results[i].name
+        print(dr)
 
     def test_run(self):
         tour_1 = Tournament(90,self.run_1 , self.run_3)
         self.all_results.update(tour_1.start())
-        self.assertTrue(self.all_results[2], "Ник")
+        self.assertTrue(self.all_results[len(self.all_results)] == "Ник")
 
     def test_run_2(self):
         tour_2 = Tournament(90, self.run_2, self.run_3)
         self.all_results.update(tour_2.start())
-        self.assertTrue(self.all_results[2], "Ник")
+        self.assertTrue(self.all_results[len(self.all_results)] =="Ник")
 
     def test_run_3(self):
         tour_3 = Tournament(90, self.run_1, self.run_2, self.run_3)
         self.all_results.update(tour_3.start())
-        self.assertTrue(self.all_results[3], self.run_3)
+        self.assertTrue(self.all_results[len(self.all_results)] == "Ник")
 
 if __name__ == "__main__":
     unittest.main()
