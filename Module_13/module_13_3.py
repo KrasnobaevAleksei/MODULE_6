66ca2a3662ffd296e85ffb923c599f6db0b77338
Aleksei <krasnobaev07@inbox.ru>
2024-12-16 22:19:20 +0500

    First Commit

---
+++ Module_13/module_13_3.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, executor, types
+# из блока работы с памятью
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+# Токен
+api = ""
+# Объект бота
+bot = Bot(token = api)
+#
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+# Пишем в строке в тееге /start(Команда) тогда обрабатывается
+@dp.message_handler(commands='start')
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.' )
+# Выводится сообщение когда в телеге пришло
+# сообщение обрабатывает все сообщения входящие
+@dp.message_handler()
+async def all_message(message):
+    await message.answer( 'Введите команду /start, чтобы начать общение.')
+# Запускаем
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates= True)
