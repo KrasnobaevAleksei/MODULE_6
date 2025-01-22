3fa049344e18d9bc88eebfbbde1da00c1ca4da1a
Aleksei <krasnobaev07@inbox.ru>
2025-01-22 08:02:36 +0500

    Second Commit

--- Module_16/module_16_1.py
+++ Module_16/module_16_1.py
@@ -6,22 +6,22 @@ app = FastAPI()
 # Когда в адресной строке набираешь(вводишь запрос) прямой слеш, в главном окне выводится надпись из return
 @app.get("/")
 async def main_page():
-    return ("Главная страница")
+    return "Главная страница"
 
 # Когда в адресной строке набираешь(вводишь запрос) "/user/admin", в главном окне выводится надпись из return
 @app.get("/user/admin")
 async def admin_page():
-    return ("Вы вошли как администратор")
+    return "Вы вошли как администратор"
 
 # Когда в адресной строке набираешь(вводишь запрос) "/user/123",
 # в главном окне выводится надпись из return с переменной {user_id}
 @app.get("/user/{user_id}")
 async def user_page(user_id: int):
-    return (f"Вы вошли как пользователь № {user_id}")
+    return f"Вы вошли как пользователь № {user_id}"
 
 # Когда в адресной строке набираешь(вводишь запрос) "/user?username = Alex& age = 12",
 # в главном окне выводится надпись из return с Alex 12
 @app.get("/user")
 async def user_data(username : str, age: int):
-    return (f"Информация о пользователе. Имя : {username}, Возраст : {age}")
+    return f"Информация о пользователе. Имя : {username}, Возраст : {age}"
 
